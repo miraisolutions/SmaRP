6eba57ea3f790a2f1aaee328c59e6760e36867f2
Gabriel <gabriel.foix@mirai-solutions.com>
2018-03-02 10:17:09 +0100

    Add tabPanel inside sidebarPanel
    Add tabPanel inside sidebarPanel. now Swiss case and General Case.
    No functionality yet

--- ui.R
+++ ui.R
@@ -1,11 +1,8 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+# SmarP UI
+# runApp()
 #
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 source("external_inputs.R")
@@ -20,36 +17,47 @@ shinyUI(
     sidebarLayout(
       sidebarPanel(
         width = 3,
-        selectInput("kanton", "Basic Info",
-                    choices = Kanton.list ,
-                    selected = "ZH"),
-        radioButtons("genre", label = NULL, inline = TRUE,
-                     choices = list("Male" = "M", "Female" = "F"), 
-                     selected = "M"),
-        selectInput("tariff", label = NULL, 
-                    choices = tariffs.list, 
-                    selected = "TA"),
-        radioButtons("NKids", label = NULL, inline = TRUE,
-                     choices = Kids.list,
-                     selected = "0kid"),
-        
-        dateInput("birthdate", label = h5("Birthday"), value = "1980-12-31", format = "yyyy-mm-dd"),
-        #      HTML('<hr style="color: black;">'),
-        hr(),
-        tags$div(class="header", checked=NA, tags$p("Pilar 2")),
-        numericInput("Salary", label = h5("Current Annual Salary"), value = 100000, step = 1000, min = 0),
-        numericInput("CurrentP2", label = h5("Current Pilar 2 amount"), value = 100000, step = 1000, min = 0),
-        numericInput("SalaryGrowthRate", label = h5("Expected salary growth rate"), value = 0.02, step = 0.001, min = 0),
-        numericInput("P2purchase", label = h5("Pilar 2 purchase"), value = 0, step = 500, min = 0),
-        radioButtons("TypePurchase", label = NULL, inline = TRUE,
-                     choices = Purchase.list),
-        hr(),
-        tags$div(class="header", checked=NA, tags$p("Pilar 3")),
-        numericInput("CurrentP3", label = h5("Current Pilar 3 amount"), value = 50000, step = 1000, min = 0),
-        numericInput("P3purchase", label = h5("Annual Pilar 3 purchase"), value = 0, step = 500, min = 0),
-        numericInput("returnP3", label = h5("Expected Return Pilar 3"), value = BVGMindestzinssatz, step = 0.001, min = 0)
-        
-        
+        tabsetPanel(
+          type = "pills",
+          tabPanel("Swiss case",
+                   
+                   selectInput("kanton", "Basic Info",
+                               choices = Kanton.list ,
+                               selected = "ZH"),
+                   radioButtons("genre", label = NULL, inline = TRUE,
+                                choices = list("Male" = "M", "Female" = "F"), 
+                                selected = "M"),
+                   selectInput("tariff", label = NULL, 
+                               choices = tariffs.list, 
+                               selected = "TA"),
+                   radioButtons("NKids", label = NULL, inline = TRUE,
+                                choices = Kids.list,
+                                selected = "0kid"),
+                   
+                   dateInput("birthdate", label = h5("Birthday"), value = "1980-12-31", format = "yyyy-mm-dd"),
+                   #      HTML('<hr style="color: black;">'),
+                   hr(),
+                   tags$div(class="header", checked=NA, tags$p("Pilar 2")),
+                   numericInput("Salary", label = h5("Current Annual Salary"), value = 100000, step = 1000, min = 0),
+                   numericInput("CurrentP2", label = h5("Current Pilar 2 amount"), value = 100000, step = 1000, min = 0),
+                   numericInput("SalaryGrowthRate", label = h5("Expected salary growth rate"), value = 0.02, step = 0.001, min = 0),
+                   numericInput("P2purchase", label = h5("Pilar 2 purchase"), value = 0, step = 500, min = 0),
+                   radioButtons("TypePurchase", label = NULL, inline = TRUE,
+                                choices = Purchase.list),
+                   hr(),
+                   tags$div(class="header", checked=NA, tags$p("Pilar 3")),
+                   numericInput("CurrentP3", label = h5("Current Pilar 3 amount"), value = 50000, step = 1000, min = 0),
+                   numericInput("P3purchase", label = h5("Annual Pilar 3 purchase"), value = 0, step = 500, min = 0),
+                   numericInput("returnP3", label = h5("Expected Return Pilar 3"), value = BVGMindestzinssatz, step = 0.001, min = 0)
+                   
+                   
+          ), #  end Swiss tabPanel
+            tabPanel("General case", 
+                     numericInput("Salary", label = h5("Current Annual Salary"), value = 100000, step = 1000, min = 0)      
+          
+            ) # end General tabPanel
+          
+        ) # end tabsetPanel
         
       ), # end sidebarPanel
       
@@ -76,7 +84,7 @@ shinyUI(
                                       font-size: 12px;
                                       text-align: left;
                                       }"))
-          ) , # end tab Plot
+          ), # end tab Plot
           
           tabPanel("Table", 
                    htmlOutput("table")
