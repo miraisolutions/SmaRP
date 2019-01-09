fa4b03a7fa4d1699fe0117abd61edf7d5c02efdc
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2019-01-09 17:35:24 +0100

    Review documentation (#57).

--- R/get_SmaRP_version.R
+++ R/get_SmaRP_version.R
@@ -1,10 +1,11 @@
-#' @title get_SmaRP_version
-#' 
+#' @title SmaRP version
+#'
 #' @rdname get_SmaRP_version
-#' 
-#' @description Automatically update SmaRP version.
-#' 
-#' @return Current SmaRP version.
+#'
+#' @description Returns the version of the installed SmaRP package and app.
+#'
+#' @return The version of SmaRP as character string.
+#'
 #' @export
 get_SmaRP_version <- function() {
   as.character(utils::packageVersion("SmaRP"))
