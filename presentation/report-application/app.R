d2f32e36cfe222df83179191fdcfe05feebb2d2c
fvitalini <francesca.vitalini@mirai-solutions.com>
2019-03-15 11:01:17 +0100

    improved presentation material

--- presentation/report-application/app.R
+++ presentation/report-application/app.R
@@ -2,7 +2,8 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  img(src = "./report.png", align = "center")
+  #img(src = "./report.png", align = "center")
+  tags$iframe(style = "height:600px; width:100%", src = "./SmaRPreport.20190315.pdf")
 )
 
 # Define server logic required to draw a histogram
